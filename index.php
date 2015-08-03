5b9a6f94889a134d9d1f60ee0d75b2f2e19412da
Edmundo Junior <hello@edmundojr.com>
2015-08-03 21:31:07 +0100

    Add test images

--- index.php
+++ index.php
@@ -5,16 +5,35 @@
 		<!-- Post -->
 		<article class="post">
 			<header>
-				<h1 class="post__title"><a href="">Lorem ipsum dolor sit amet</a></h1>
+				<h1 class="post__title"><a href="">The Wildside</a></h1>
 			</header>
-			<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper.</p>
-			<p>Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis.</p>
-			<h2>Fusce dapibus, tellus ac cursus commodo</h2>
-			<p>Curabitur blandit tempus porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod.</p>
-			<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
-			<h3>Fusce dapibus, tellus ac cursus commodo</h3>
-			<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas faucibus mollis interdum.</p>
-			<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla.</p>
+			<p>Last weekend Laura and I went camping in the Wicklow Mountains. It was Laura's first time sleeping on a tent, and I could see that although excited, she seemed scared too.</p>
+			<p>As for me, I've always loved the great outdoors, even though I hadn't had a chance to see the <em>really great outdoors</em> with mountains that go all the way through the clouds.</p>
+			<p>Laura on the other hand is more of a city-girl. She likes comfort as much as the next city-girl: sleep in a soft bed, take a warm bath and enjoy her meals <em>preferably</em> hot. By all means, there's nothing wrong with that, we're just different in this matter.</p>
+			<p>So when she told me she wanted go along with me in one of my hikes, I felt surprised, but very content, for is nice to have a partner with you on long hikes – specially someone that <em>is</em> your partner. We decided to go on a two day hike to Glendalough, Co. Wicklow.</p>
+			<p>Backpacks packed, shoes ready, sandwiches sandwiched, canteen filled: it was time to go.</p>
+			<figure class="pic clearfix">
+				<a href="">
+					<img src="images/the-wildside/the-wildside-1.jpg" alt="">
+				</a>
+				<a class="col-1-2" href="">
+					<img src="images/the-wildside/the-wildside-2.jpg" alt="">
+				</a>
+				<a class="col-1-2" href="">
+					<img src="images/the-wildside/the-wildside-3.jpg" alt="">
+				</a>
+			</figure>
+			<h2>Powerscout Waterfall</h2>
+			<p>We start the journey in Enniskerry, a small village 5 minutes south of the Dublin border, in the foothills of the Wicklow Mountains. We were planning to walk from there to Powerscourt Gardens and then take a trail to Powerscourt Waterfall. Sadly our map was inaccurate, and the trails and roads on the Gardens were private. We had to take a massive detour, but since we're in Ireland, the route was very charming.</p>
+			<p>We were famished by the time we reached the Waterfall, so I ordered cheese and chips to eat with the sandwiches. They have a little kiosk that sells almost anything you'd expected to find in a kiosk – maybe a bit more.</p>
+			<p>Powerscourt Waterfall is splendidly beautiful, at 121m is Ireland's highest waterfall. The weather was nice and warm, so there was loads of people enjoying some sun time. We relaxed and took some pictures, while getting mentally ready for the next round.</p>
+			<h2>Djouce Woods and Djouce Mountain</h2>
+			<p>Upon arriving in the waterfall, we asked the gatekeeper if it was possible to walk through Djouce Woods’ tracks. He said that the way was closed, but there was a section of the fence that had fell over, and maybe we could try there: “Just go over it”, he said.</p>
+			<p>It was there that our adventure truly begun, for the tracks were all old and unused, and there was so many of them that I had to consult the map all the time. We're tired and eager to get to the campsite before nighttime, so we pressed ourselves to keep going. Finally we found our way to the other side and to the Wicklow Way, what a relieve!</p>
+			<p>We set up our tent amongst millions of midges and other bugs – seriously, I'd never seen so many insects together – on the base of Djouce Mountain (380m),  right on the middle of that spectacular scenery! You could see the Irish sea between Bray and Greystones, just astonishing.</p>
+			<p>The rain had not stopped all night, and then <strong>we heard the screams</strong>: angry or maybe terrified, one guy was searching/hunting someone, we still don't know each one it was for sure. That startled us for a bit, so I went outside to check the whereabouts. I saw a couple of guys on top of the Maulin Mountain, (probably) searching for a missing friend which apparently they'd found, because all of a sudden the screams stopped.</p>
+			<h2>The Invisible Lough Tay</h2>
+			<p>Early next morning we broke camp and moved on to the Wicklow Way. It was a cool and windy morning when we start moving. Unfortunately started to rain and in 5 minutes time we were soaking wet. The rain and fog persisted through the mountains, so Laura wasn't able to see <strong>Lough Tay</strong>, the beautiful lake that is part owned by the Guinness Family, and where History Channel recorded their hit series Vikings.</p>
 			<footer class="post__meta">
 				<time class="post__meta__date">Jan 15, 2015</time>
 				<span class="spacer">·</span>
